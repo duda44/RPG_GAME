9f3843cfdbd1841cb866b8100286313a48afdca5
AlemTuzlak <t.zlak@hotmail.com>
2017-01-03 11:48:03 +0100

    Small upgrades and fixes
    Has a start screen now

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 const int PLAYER_SIZE = 6;
 const int LOOT_SIZE = 4;
 void player()
 {
-	// HP(0), Mana(1),  ATK(2); defense(3), player_exp(4), player_level(5)
-	int player_stats[PLAYER_SIZE] = { 100, 100, 15, 1, 0, 1 };
+
 	// HP_potion(0), MP potion(1), attack_increase_potion(2), revival_potion(3)
 	int loot[LOOT_SIZE] = { 0, 0, 0, 0 };
 
@@ -44,8 +45,31 @@ bool level_up(int player_stats[])
 	}
 	return level_up;
 }
+//randomly spawns a creature to fight
+bool encounters()
+{
+	return rand() & 2;
+}
+void print_screen(int player_stats[])
+{
+	cout << "HP:" << player_stats[0] << "		Mana:" << player_stats[1] << "		Exp:" << player_stats[4] << "		Level:" << player_stats[5];
+	for (int i = 0; i < 11; i++)
+		cout << endl;
+	cout << "		WHERE DO YOU WANT TO GO?";
+	for (int i = 0; i < 11; i++)
+		cout << endl;
+	if (encounters)
+		cout << "1.Attack    	2.Potions		3.Flee		4.Defend" << endl;
+	
+}
 int main()
 {
-	int player_level = 1;
+	// HP(0), Mana(1),  ATK(2); defense(3), player_exp(4), player_level(5)
+	int player_stats[PLAYER_SIZE] = { 100, 100, 15, 1, 0, 1 };
+	srand(time(0));
+	print_screen(player_stats);
+	system("pause");
+	return 0;
+
 
 }
