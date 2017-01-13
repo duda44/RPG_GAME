b709a47dcaeeec80ff7958c79159bce989956e67
AlemTuzlak <t.zlak@hotmail.com>
2017-01-13 16:07:05 +0100

    Main menu(bug-free)
    Main menu, load game and new game without any bugs. Save game not tested
    yet so not declared as fully functional. 2 files added, one for players
    second one for saving games which will be used later

--- Source1.cpp
+++ Source1.cpp
@@ -3,10 +3,12 @@
 #include <ctime>
 #include<stdio.h>
 #include<conio.h>
-
+#include <stdlib.h> 
+#include <fstream>
+#include <string>
 using namespace std;
 
-int ID, HP, Mana, ATK, Defense, player_exp, player_level;
+int ID, HP, Mana, ATK, ability_dmg, Defense, magic_resist, player_exp, player_level;
 string Name, Class;
 //void player()
 //{
@@ -22,7 +24,7 @@ void credits();
 void main_menu();
 void new_player();
 void load_game();
-
+void save_game();
 //void print_screen(int player_stats[])
 //{
 //	cout << "HP:" << player_stats[0] << "		Mana:" << player_stats[1] << "		Exp:" << player_stats[4] << "		Level:" << player_stats[5];
@@ -35,6 +37,8 @@ void load_game();
 //		cout << "1.Attack    	2.Potions		3.Flee		4.Defend" << endl;
 //
 //}
+
+//fully functional
 int main()
 {
 	main_menu();
@@ -43,6 +47,7 @@ int main()
 
 
 }
+//fully functional
 void main_menu()
 {
 	bool menu = true;
@@ -72,11 +77,12 @@ void main_menu()
 		case 3:
 			credits();
 			break;
-		default:
+		case 4:
 			menu = false;
 		}
 	} while (menu);
 }
+//fully functional
 void credits()
 {
 	int choice;
@@ -95,40 +101,49 @@ void credits()
 	system("cls");
 	main_menu();
 }
+//fully functional
 void new_player()
 {
-	ifstream new_player_input("Players.txt");
-	ofstream new_player_output("Players.txt");
+	ifstream new_player_input("Players.txt") ;
+	ofstream new_player_output;
+	new_player_output.open("Players.txt" , ios::app);
 	int counter = 0;
-	while (new_player_input >> ID >> Name >> Class >> HP >> Mana >> ATK >> Defense >> player_exp >> player_level)
+	new_player_input.clear();
+	new_player_input.seekg(0, new_player_input.beg);
+	while (new_player_input >> ID >> Name >> Class >> HP >> Mana >> ATK >> ability_dmg >> Defense >> magic_resist >> player_exp >> player_level)
 	{
 		counter++;
-		if (new_player_input.eof())
-			new_player_output << counter;
-
 	}
-	cout << "Enter your name";
+
+	new_player_output << counter << ' ';
+	cout << "	   The last son of Allheaven				" << endl;
+	cout << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" << endl;
+	cout << "Enter your name: ";
 	cin >> Name;
 	new_player_output << Name;
 	cout << "Hello " << Name << " you have successfully entered your name! Now to the next quest!";
+	cout << endl;
+	cout << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" << endl;
+	system("pause");
 	system("cls");
 	cout << "Choose your class:" << endl;
 	cout << "1. Warrior(Warrior has high hp, defense and magic resistance but average damage and ability damage and low mana)" << endl;
-	cout << "2. Archer (Archer has very high attack damage but has average ability damage, hp and mana but low defense and magic resistance." << endl;
-	cout << "3. Mage (Mage has very high ability damage, mana and magic resistance, average hp but has low attack and defense." << endl;
+	cout << endl << "2. Archer (Archer has very high attack damage but has average ability damage, hp and mana but low defense and magic resistance." << endl;
+	cout << endl << "3. Mage (Mage has very high ability damage, mana and magic resistance, average hp but has low attack and defense." << endl;
+	int choice;
 	cin >> choice;
 	system("cls");
 
 	switch (choice)
 	{
 	case 1:
-		new_player_output << "Warrior" << 150 << 60 << 20 << 20 << 5 << 5 << 0 << 1;
+		new_player_output << ' ' << "Warrior" << ' ' << 150 << ' ' << 60 << ' ' << 20 << ' ' << 20 << ' ' << 5 << ' ' << 5 << ' ' << 0 << ' ' << 1 << endl;
 		break;
 	case 2:
-		new_player_output << "Archer" << 100 << 100 << 40 << 20 << 1 << 1 << 0 << 1;
+		new_player_output << ' ' << "Archer" << ' ' << 100 << ' ' << 100 << ' ' << 40 << ' ' << 20 << ' ' << 1 << ' ' << 1 << ' ' << 0 << ' ' << 1 << endl;
 		break;
 	case 3:
-		new_player_output << "Mage" << 100 << 200 << 10 << 60 << 1 << 7 << 0 << 1;
+		new_player_output << ' ' << "Mage" << ' ' << 100 << ' ' << 200 << ' ' << 10 << ' ' << 60 << ' ' << 1 << ' ' << 7 << ' ' << 0 << ' ' << 1 << endl;
 		break;
 	default:
 		cout << "You have entered an invalid choice";
@@ -137,15 +152,17 @@ void new_player()
 	new_player_output.close();
 	cout << "You have successfully created a new characher, enjoy the game!" << endl;
 }
+//fully functional
 void load_game()
 {
 	bool loading = true;
 	do{
 		ifstream in_stream("Players.txt");
-		ofstream out_stream("Players.txt");
 		cout << "	   The last son of Allheaven				" << endl;
 		cout << "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" << endl;
 		char ch;
+		in_stream.clear();
+		in_stream.seekg(0, in_stream.beg);
 		while (in_stream.get(ch))
 		{
 			cout << ch;
@@ -157,28 +174,66 @@ void load_game()
 		in_stream.seekg(0, in_stream.beg);
 		int choice;
 		cin >> choice;
-		while (in_stream >> ID >> Name >> Class >> HP >> Mana >> ATK >> Defense >> player_exp >> player_level)
+		while (in_stream >> ID >> Name >> Class >> HP >> Mana >> ATK >> ability_dmg >> Defense >> magic_resist >> player_exp >> player_level)
 		{
 			if (choice == ID)
 			{
-				game_start();
+				/*game_start();*/
 				loading = false;
 				break;
-			}
-			else if (in_stream.eof())
-			{
-				in_stream.clear();
-				in_stream.seekg(0, in_stream.beg);
-				cout << endl << "Error! Invalid ID !" << endl;
-				
 			}
 			else if (choice == -1)
 			{
 				system("cls");
+				loading = false;
 				main_menu();
 			}
+			else if (choice > ID && in_stream.eof())
+			{
+				cout << endl << "Error! Invalid ID !" << endl;
+				system("cls");
+				in_stream.clear();
+				in_stream.seekg(0, in_stream.beg);
+			}
+			else
+				system("cls");
 		}
 		in_stream.close();
-		out_stream.close();
-	} while (loading)
+	} while (loading);
+}
+//has to be tested
+void save_game()
+{
+	ifstream in_stream("Players.txt");
+	ofstream out_stream("Players.txt");
+	ifstream temp_in("temp.txt");
+	ofstream temp_out("temp.txt");
+
+	int temp_ID, temp_HP, temp_Mana, temp_ATK, temp_Defense, temp_player_exp, temp_player_level;
+	char temp_Name[50], temp_Class[15];
+	while (in_stream >> temp_ID >> temp_Name >> temp_Class >> temp_HP >> temp_Mana >> temp_ATK >> temp_Defense >> temp_player_exp >>temp_player_level)
+	{
+		if (ID == temp_ID)
+			temp_out << ID << ' ' << Name << ' ' << Class << ' ' << HP << ' ' << Mana << ' ' << ATK << ' ' << Defense << ' ' << player_exp << ' ' << player_level;
+		
+		else
+			temp_out << ID << ' ' << Name << ' ' << Class << ' ' << temp_HP << ' ' << temp_Mana << ' ' << temp_ATK << ' ' << temp_Defense << ' ' << temp_player_exp << ' ' << temp_player_level;
+		
+	}
+	
+	in_stream.close();
+	out_stream.close();
+
+	ofstream ostream;
+	ostream.open("Players.txt", std::ofstream::out | std::ofstream::trunc);
+	char ch;
+	while (temp_in.get(ch))
+	{
+		ostream << ch;
+	}
+
+	ostream.close();
+	temp_in.close();
+	temp_out.close();
+
 }
